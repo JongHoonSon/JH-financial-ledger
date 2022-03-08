615b8df75b55bc5d9e6af23823606fef9f9af414
JongHoonSon <roka1684@gmail.com>
2022-03-08 17:19:18 +0900

    Add bookkeep function

---
+++ js/bookkeeping.js
@@ -0,0 +1,64 @@
+const $incomeList = document.querySelector(".income-list");
+const $expenseList = document.querySelector(".expense-list");
+
+const $addItemFormInputTitle = document.querySelector(".addItem__form__input__title");
+const $addItemFormInputAmount = document.querySelector(".addItem__form__input__amount");
+
+const $addItemFormBtnIncome = document.querySelector(".addItem__btn__income");
+const $addItemFormBtnExpense = document.querySelector(".addItem__btn__expense");
+
+$addItemFormBtnIncome.addEventListener("click", addItem);
+$addItemFormBtnExpense.addEventListener("click", addItem);
+
+function addItem(event) {
+    const newItemTitle = $addItemFormInputTitle.value;
+    const newItemAmount = $addItemFormInputAmount.value;
+
+    if(newItemTitle.length === 0 && newItemAmount.length === 0) {
+        alert('내용과 금액을 입력하세요.');
+        return;
+    } else if(newItemTitle.length === 0) {
+        alert('내용을 입력하세요.');
+        return;
+    } else if(newItemAmount.length === 0) {
+        alert('금액을 입력하세요.');
+        return;
+    }
+    
+    const $newListItem = document.createElement("li");
+    const $newListItemDate = document.createElement("span");
+    const $newListItemTitle = document.createElement("span");
+    const $newListItemAmount = document.createElement("span");
+    
+    const date = new Date()
+    const thisMonth = date.getMonth()+1;
+    const thisDate = date.getDate();
+
+    $newListItemDate.innerText = `${thisMonth}/${thisDate}`;
+    $newListItemTitle.innerText = newItemTitle;
+    $newListItemAmount.innerText = newItemAmount;
+
+    $newListItem.appendChild($newListItemDate);
+    $newListItem.appendChild($newListItemTitle);
+    $newListItem.appendChild($newListItemAmount);
+    
+    
+    const clickedBtnName = event.path[0].innerText;
+    let classNamePrefix;
+
+    if(clickedBtnName === 'INCOME') {
+        classNamePrefix = "income";
+        $incomeList.appendChild($newListItem);
+    } else if(clickedBtnName === "EXPENSE") {
+        classNamePrefix = "expense";
+        $expenseList.appendChild($newListItem);
+    }
+
+    $newListItem.classList.add(`${classNamePrefix}-list__item`);
+    $newListItemDate.classList.add(`${classNamePrefix}-list__item__date`);
+    $newListItemTitle.classList.add(`${classNamePrefix}-list__item__title`);
+    $newListItemAmount.classList.add(`${classNamePrefix}-list__item__amount`);
+
+    $addItemFormInputTitle.value = '';
+    $addItemFormInputAmount.value = '';
+}
